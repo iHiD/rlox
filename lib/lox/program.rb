8f9f2a40aa0624cae0a3ef667422802f07f6cf84
Jeremy Walker <jez.walker@gmail.com>
2022-01-09 20:01:49 +0000

    Define token types and split program into its own class

---
+++ lib/lox/program.rb
@@ -0,0 +1,64 @@
+module Lox
+  class Program
+    def self.exec!(args)
+      new(args).exec!
+    end
+
+    def initialize(args)
+      @args = args
+    end
+
+    def exec!
+      if(args.length > 1)
+        puts "Usage: lox [script]"
+        exit(64)
+      elsif args.length == 1
+        run_file(args[0])
+      else
+        run_prompt
+      end
+    end
+
+    private
+    attr_reader :args, :had_error
+
+    def run_file(filepath)
+      run(File.read(filepath))
+
+      System.exit(65) if had_error
+    end
+
+    def run_prompt
+      puts "Running prompt"
+
+      loop do
+        print "> "
+        line = STDIN.gets
+        break unless line
+
+        run(line)
+
+        @had_error = false
+      end
+    end
+
+    def run(source)
+      scanner = Scanner.new(source)
+      tokens = scanner.scan_tokens
+
+      puts "Tokens:"
+      tokens.each do |token|
+        puts "token"
+      end
+    end
+
+    def error(line, msg)
+      report(line, "", msg)
+    end
+
+    def report(line, where, message)
+      puts "[line #{line}] Error #{where}: #{message}"
+      @had_error = true
+    end
+  end
+end
